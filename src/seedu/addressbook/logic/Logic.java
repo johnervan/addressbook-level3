f863c9cef3d2cf583dd8af330ab6b48eee181806
johnervan <johnervan@u.nus.edu>
2016-09-11 10:08:08 +0800

    Add condition to execute command in Logic.java to save data only if command mutates the data

--- src/seedu/addressbook/logic/Logic.java
+++ src/seedu/addressbook/logic/Logic.java
@@ -16,84 +16,95 @@ import java.util.Optional;
  */
 public class Logic {
 
+	private StorageFile storage;
+	private AddressBook addressBook;
 
-    private StorageFile storage;
-    private AddressBook addressBook;
+	/** The list of person shown to the user most recently. */
+	private List<? extends ReadOnlyPerson> lastShownList = Collections.emptyList();
 
-    /** The list of person shown to the user most recently.  */
-    private List<? extends ReadOnlyPerson> lastShownList = Collections.emptyList();
+	public Logic() throws Exception {
+		setStorage(initializeStorage());
+		setAddressBook(storage.load());
+	}
 
-    public Logic() throws Exception{
-        setStorage(initializeStorage());
-        setAddressBook(storage.load());
-    }
+	Logic(StorageFile storageFile, AddressBook addressBook) {
+		setStorage(storageFile);
+		setAddressBook(addressBook);
+	}
 
-    Logic(StorageFile storageFile, AddressBook addressBook){
-        setStorage(storageFile);
-        setAddressBook(addressBook);
-    }
+	void setStorage(StorageFile storage) {
+		this.storage = storage;
+	}
 
-    void setStorage(StorageFile storage){
-        this.storage = storage;
-    }
+	void setAddressBook(AddressBook addressBook) {
+		this.addressBook = addressBook;
+	}
 
-    void setAddressBook(AddressBook addressBook){
-        this.addressBook = addressBook;
-    }
+	/**
+	 * Creates the StorageFile object based on the user specified path (if any)
+	 * or the default storage path.
+	 * 
+	 * @throws StorageFile.InvalidStorageFilePathException
+	 *             if the target file path is incorrect.
+	 */
+	private StorageFile initializeStorage() throws StorageFile.InvalidStorageFilePathException {
+		return new StorageFile();
+	}
 
-    /**
-     * Creates the StorageFile object based on the user specified path (if any) or the default storage path.
-     * @throws StorageFile.InvalidStorageFilePathException if the target file path is incorrect.
-     */
-    private StorageFile initializeStorage() throws StorageFile.InvalidStorageFilePathException {
-        return new StorageFile();
-    }
+	public String getStorageFilePath() {
+		return storage.getPath();
+	}
 
-    public String getStorageFilePath() {
-        return storage.getPath();
-    }
+	/**
+	 * Unmodifiable view of the current last shown list.
+	 */
+	public List<ReadOnlyPerson> getLastShownList() {
+		return Collections.unmodifiableList(lastShownList);
+	}
 
-    /**
-     * Unmodifiable view of the current last shown list.
-     */
-    public List<ReadOnlyPerson> getLastShownList() {
-        return Collections.unmodifiableList(lastShownList);
-    }
+	protected void setLastShownList(List<? extends ReadOnlyPerson> newList) {
+		lastShownList = newList;
+	}
 
-    protected void setLastShownList(List<? extends ReadOnlyPerson> newList) {
-        lastShownList = newList;
-    }
+	/**
+	 * Parses the user command, executes it, and returns the result.
+	 * 
+	 * @throws Exception
+	 *             if there was any problem during command execution.
+	 */
+	public CommandResult execute(String userCommandText) throws Exception {
+		Command command = new Parser().parseCommand(userCommandText);
+		CommandResult result = execute(command);
+		recordResult(result);
+		return result;
+	}
 
-    /**
-     * Parses the user command, executes it, and returns the result.
-     * @throws Exception if there was any problem during command execution.
-     */
-    public CommandResult execute(String userCommandText) throws Exception {
-        Command command = new Parser().parseCommand(userCommandText);
-        CommandResult result = execute(command);
-        recordResult(result);
-        return result;
-    }
+	/**
+	 * Executes the command, updates storage, and returns the result.
+	 *
+	 * @param command
+	 *            user command
+	 * @return result of the command
+	 * @throws Exception
+	 *             if there was any problem during command execution.
+	 */
+	private CommandResult execute(Command command) throws Exception {
+		command.setData(addressBook, lastShownList);
+		CommandResult result = command.execute();
+		if (command.isMutating()) {
+			storage.save(addressBook);
+		}
+		return result;
+	}
 
-    /**
-     * Executes the command, updates storage, and returns the result.
-     *
-     * @param command user command
-     * @return result of the command
-     * @throws Exception if there was any problem during command execution.
-     */
-    private CommandResult execute(Command command) throws Exception {
-        command.setData(addressBook, lastShownList);
-        CommandResult result = command.execute();
-        storage.save(addressBook);
-        return result;
-    }
-
-    /** Updates the {@link #lastShownList} if the result contains a list of Persons. */
-    private void recordResult(CommandResult result) {
-        final Optional<List<? extends ReadOnlyPerson>> personList = result.getRelevantPersons();
-        if (personList.isPresent()) {
-            lastShownList = personList.get();
-        }
-    }
+	/**
+	 * Updates the {@link #lastShownList} if the result contains a list of
+	 * Persons.
+	 */
+	private void recordResult(CommandResult result) {
+		final Optional<List<? extends ReadOnlyPerson>> personList = result.getRelevantPersons();
+		if (personList.isPresent()) {
+			lastShownList = personList.get();
+		}
+	}
 }
