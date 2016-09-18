93e0909d6085f987d50dcb6eae04162cff068caf
johnervan <johnervan@u.nus.edu>
2016-09-18 21:33:38 +0800

    edit StorageFile.java to implement Storage interface

--- src/seedu/addressbook/storage/StorageFile.java
+++ src/seedu/addressbook/storage/StorageFile.java
@@ -15,7 +15,7 @@ import java.nio.file.Paths;
 /**
  * Represents the file used to store address book data.
  */
-public class StorageFile {
+public class StorageFile implements Storage {
 
     /** Default file path used if the user doesn't provide the file name. */
     public static final String DEFAULT_STORAGE_FILEPATH = "addressbook.txt";
@@ -144,5 +144,6 @@ public class StorageFile {
     public String getPath() {
         return path.toString();
     }
+
 
 }
